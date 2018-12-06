075fef63dece3a26f359f4eea6f90a179ebfb1b4
susereum <susereum@gmail.com>
2018-12-06 14:27:25 -0700

    Add files via upload

---
+++ JavaArrayStoreException.java
@@ -0,0 +1,11 @@
+/* ............... START ............... */
+                
+public class JavaArrayStoreException {
+
+	public static void main(String... args) {
+		Object[] val = new Integer[4];
+		val[0] = 5.8;
+	}
+
+}
+/* ............... END ............... */
